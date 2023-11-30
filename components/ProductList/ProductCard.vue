0c1aa92ccb8053090edf08137923f05ea1c6e800
Jenny <jenny.maya186@gmail.com>
2023-11-30 14:20:47 +0700

    Link added

--- components/ProductList/ProductCard.vue
+++ components/ProductList/ProductCard.vue
@@ -1,14 +1,17 @@
 <template>
   <div
-    class="text-xs shadow-green-400 shadow-sm m-5 p-5 w-[250px] h-[400px] flex flex-col justify-between hover:shadow-md hover:shadow-blue-600 transition-shadow ease-in-out">
+    class="text-xs shadow-green-400 shadow-sm m-5 p-5 w-[250px] h-[400px] flex flex-col justify-between hover:shadow-md hover:shadow-blue-600 transition-shadow ease-in-out"
+  >
     <div>Images Slider</div>
     <div class="flex flex-col justify-between items-center">
       <span class="text-[15px] flex justify-start w-full">
-        <b>{{
-          product.title.length > 22
-            ? `${product.title.slice(0, 22)}.....`
-            : product.title
-        }}</b>
+        <ULink :to="url">
+          <b>{{
+            product.title.length > 22
+              ? `${product.title.slice(0, 22)}.....`
+              : product.title
+          }}</b></ULink
+        >
       </span>
       <span class="flex justify-between items-center w-full text-[13px] mt-2">
         <div class="flex gap-1 items-center">
@@ -25,4 +28,6 @@
 <script setup>
 const props = defineProps(["data"]);
 const product = ref(props.data);
+
+const url = "/products/"+product?.value?.id;
 </script>
