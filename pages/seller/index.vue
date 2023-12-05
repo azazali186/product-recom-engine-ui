aaa3db78bd82d87f51ee297ba9523c6fcbfd7a81
Jenny <jenny.maya186@gmail.com>
2023-12-05 11:09:40 +0700

    Seller Login and register Created

---
+++ pages/seller/index.vue
@@ -0,0 +1,6 @@
+﻿<template>
+    <div class="flex justify-center gap-5 text-4xl font-bold items-center h-container">
+        <ULink to="/seller/login" class="font-bold text-blue-400 hover:text-green-600 hover:scale-110 cursor-pointer transition ease-in-out">Login</ULink>
+        <ULink to="/seller/register" class="font-bold text-blue-400 hover:text-green-600 hover:scale-110 cursor-pointer transition ease-in-out">Register</ULink>
+    </div>
+</template>
