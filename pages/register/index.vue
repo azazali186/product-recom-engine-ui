60461975943877862d908bc7446797de730d8755
Jenny <jenny.maya186@gmail.com>
2023-12-05 10:51:59 +0700

    Design Login and register page

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,10 +1,23 @@
-<template>
+﻿<template>
   <div class="flex justify-center items-center h-container">
     <div
-      class="login-card border-1 rounded-lg border-green-400 shadow-sm shadow-green-400 flex flex-col gap-10 p-10 hover:shadow-md hover:shadow-blue-600"
+      class="register-card border-1 rounded-lg border-green-400 shadow-sm shadow-green-400 flex flex-col gap-10 p-10 hover:shadow-md hover:shadow-blue-600"
     >
       <div class="text-4xl font-bold flex justify-center items-center">
-        Login
+        Register
+      </div>
+      <div class="flex gap-5 w-[100%] justify-center items-center">
+        <span class="login-input-lable">Telephone: </span>
+        <span class="login-input-box"
+          ><UInput
+            v-model="mobileNumber"
+            autocomplete="off"
+            placeholder="+855********"
+          >
+            <template #leading>
+              <IconsTelephone />
+            </template> </UInput
+        ></span>
       </div>
       <div class="flex gap-5 w-[100%] justify-center items-center">
         <span class="login-input-lable">Email: </span>
@@ -43,17 +56,17 @@
       </div>
       <div class="flex justify-center items-center">
         <button
-          class="w-[80px] relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+          class="w-[100px] relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
         >
           <span
             class="relative px-5 py-2.5 hover:text-white font-bold dark:text-white transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
           >
-            Login
+            Register
           </span>
         </button>
       </div>
       <div class="flex justify-center gap-2 items-center">
-        Don't have account <ULink to="/register" class="hover:text-green-400 font-bold"> Register Here </ULink>
+        Already have account to <ULink to="/login" class="hover:text-green-400 font-bold"> Login Here </ULink>
       </div>
     </div>
   </div>
@@ -62,5 +75,6 @@
 <script setup>
 const username = ref();
 const password = ref();
+const mobileNumber = ref();
 const show = ref(true);
 </script>
