fd19fab5bc6bc03395001fc806f14fb14239abe0
Ali Azaz <azaz.ali186@gmail.com>
2024-01-14 13:55:47 +0700

    Search engin search functionality updated

--- components/SearchBox/SearchInputBox.vue
+++ components/SearchBox/SearchInputBox.vue
@@ -1,60 +1,107 @@
 <template>
   <div
-    :class="
-      input
-        ? 'bg-gradient-to-br from-green-400 to-blue-600' + clas
-        : 'bg-gray-300' + clas
+    style="
+      width: 100%;
+      justify-content: center;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
     "
   >
-    <div class="search-engine-search bg-white">
-      <Icon
-        class="search-engine-search-box-icon color-search"
-        name="heroicons-solid:search"
-      />
-      <input
-        v-model="search"
-        type="text"
-        class="search-engine-input-box mw-full"
-        placeholder="Search your needed products..."
-        ref="searchInput"
-      />
-
-      <!-- <USelectMenu
-        v-model="selected"
-        :options="cat"
-        placeholder="Select Category"
-        value-attribute="id"
-        option-attribute="name"
-        class="search-engine-select-box"
-        selected-icon="i-heroicons-hand-thumb-up-solid"
-        searchable
-        searchable-placeholder="Search by name or favorite colors"
-        :search-attributes="['name']"
+    <div style="width: 100%; justify-content: center; display: flex">
+      <div
+        :class="
+          input
+            ? 'bg-gradient-to-br from-green-400 to-blue-600' + clas
+            : 'bg-gray-300' + clas
+        "
       >
-        <UButton
-          color="gray"
-          class="flex-1 justify-between search-engine-select-box-button"
-        >
-          {{ current?.name || "All Categories" }}
-          <UIcon
-            name="i-heroicons-chevron-right-20-solid"
-            class="w-5 h-5 transition-transform text-gray-400 dark:text-gray-500"
-            :class="[open && 'transform rotate-90']"
+        <div class="search-engine-search bg-white">
+          <Icon
+            class="search-engine-search-box-icon color-search"
+            name="heroicons-solid:search"
           />
-        </UButton>
-        <template #label>
-          {{ current?.name || "All Categories" }}
-        </template>
-      </USelectMenu> -->
-
-      <!-- <Icon
-        class="search-engine-search-box-icon mh-30 color-mic"
-        name="heroicons-solid:microphone"
-      />
-      <Icon
-        class="search-engine-search-box-icon mh-30 color-camera"
-        name="heroicons-solid:camera"
-      /> -->
+          <input
+            v-model="search"
+            type="text"
+            class="search-engine-input-box mw-full"
+            placeholder="Search your needed products..."
+            ref="searchInput"
+          />
+        </div>
+      </div>
+    </div>
+    <div
+      style="width: 100%; justify-content: center; display: flex"
+      v-if="data"
+    >
+      <div
+        :class="
+          input
+            ? 'bg-gradient-to-br from-green-400 to-blue-600' + clas
+            : 'bg-gray-300' + clas
+        "
+      >
+        <div
+          class="search-engine-search bg-white"
+          style="flex-direction: column"
+        >
+          <h1 class="font-bold capitalize text-[24px]">search Suggestions:</h1>
+          <div
+            class="flex w-full px-5 pb-5"
+            style="justify-content: space-between"
+          >
+            <table class="w-full border-2">
+              <thead class="border-2">
+                <tr>
+                  <th v-if="prod.length>0">
+                    <table class="w-full border-2">
+                      <thead class="border-2">
+                        <tr class="border-2">
+                          <th class="border-2 font-bold  text-[18px]">Product Name</th>
+                        </tr>
+                      </thead>
+                      <tbody class="border-2">
+                        <tr class="border-2" v-for="pd in prod">
+                          <td class="border-2">{{ pd.translations[0].name }}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </th>
+                  <th v-if="category.length>0">
+                    <table class="w-full border-2">
+                      <thead class="border-2">
+                        <tr class="border-2">
+                          <th class="border-2 font-bold text-[18px]">Category Name</th>
+                        </tr>
+                      </thead>
+                      <tbody class="border-2">
+                        <tr class="border-2" v-for="cats in category">
+                          <td class="border-2">{{ cats.translations[0].name }}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </th>
+                  <th v-if="shop.length>0">
+                    <table class="w-full border-2">
+                      <thead class="border-2">
+                        <tr class="border-2">
+                          <th class="border-2 font-bold text-[18px]">Shop Name</th>
+                        </tr>
+                      </thead>
+                      <tbody class="border-2">
+                        <tr class="border-2" v-for="sp in shop">
+                          <td class="border-2">{{ sp.name }}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -64,7 +111,10 @@ import { ref, watch } from "vue";
 import { getQueryData } from "~/helpers/Utils";
 const cat = ref([]);
 const prod = ref([]);
+const category = ref([]);
+const shop = ref([]);
 const input = ref(false);
+const data = ref(false);
 const open = ref(false);
 const searchInput = ref(null);
 import store from "~/store";
@@ -95,6 +145,7 @@ const handleClickOutside = (event) => {
 };
 
 watch(search, () => {
+  data.value = false;
   if (search.value?.length > 2) {
     searchFunction();
   }
@@ -111,19 +162,34 @@ const searchFunction = async () => {
 };
 
 const productSearch = async () => {
+  data.value = false;
   try {
     const queryData = {};
     if (selected.value > 0) queryData.category_ids = selected.value;
     if (search.value.length > 2) queryData.search = search.value;
     const params = getQueryData(queryData);
     const url = "/search?" + params;
-    const res = await useCustomFetch({ url });
-    prod.value = res?.data?.list;
-    product.data = prod.value;
-    product.count = res.data.count;
-    product.query = search.value;
-    prodState.setProductData(prod.value);
-    console.log(prod.value);
+    if (search.value.length > 2) {
+      const res = await useCustomFetch({ url });
+      console.log("res", res);
+      prod.value = res?.data?.products?.list;
+      category.value = res?.data?.cats?.list;
+      shop.value = res?.data?.shops?.list;
+      product.data = prod.value;
+      product.count = res.data.products.count;
+      product.query = search.value;
+      prodState.setProductData(prod.value);
+
+      if (
+        prod.value.length > 0 ||
+        category.value.length > 0 ||
+        shop.value.length > 0
+      ) {
+        data.value = true;
+      }
+
+      console.log(prod.value);
+    }
   } catch (error) {
     console.log("err", error);
   }
--- components/SearchBox/index.vue
+++ components/SearchBox/index.vue
@@ -3,10 +3,10 @@
     <div class="w-[30%] mb-10">
       <img :src="logo" />
     </div>
-    <div class="w-full flex justify-center">
+    <div class="w-full flex justify-center" style="flex-direction: column; align-items: center;">
       <SearchInputBox @update="changeValue" />
     </div>
-    <div class="flex gap-2 mt-5 p-3">
+    <!-- <div class="flex gap-2 mt-5 p-3">
       <ULink to="/search">
         <button
         class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
@@ -28,7 +28,7 @@
           I'm Feeling Lucky
         </span>
       </button>
-    </div>
+    </div> -->
   </div>
 </template>
 
