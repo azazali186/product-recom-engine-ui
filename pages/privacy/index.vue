aa775ae2a397b40286223327f9a69721c5c472f9
Janny <yenny.maya186@gmail.com>
2024-03-12 09:19:38 +0700

    Updated the alignments

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-center p-10 items-start h-screen">
     <div class="w-[90%] h-[100%] text-gray-500 font-bold">
-      <h1>Privacy Policy</h1>
-      <h4>
+      <h1 class="text-[32px] my-2">Privacy Policy</h1>
+      <h4 class="text-[18px] my-2">
         Go-kh Free Classifieds aims to give users an engaging and customized
         experience in addition to the many benefits of internet technology.
         Subject to the guidelines in this privacy statement, we may utilize
@@ -11,7 +11,7 @@
         sold, or rented to an uninvited third party.
       </h4>
 
-      <h4>
+      <h4 class="text-[18px] my-2">
         Go-kh Free Classifieds aims to give users an engaging and customized
         experience in addition to the many benefits of internet technology.
         Subject to the guidelines in this privacy statement, we may utilize
@@ -19,41 +19,39 @@
         address, and phone number). Your email information will never be traded,
         sold, or rented to an uninvited third party.
       </h4>
+      <br />
 
-      <h2>Information Collections</h2>
-      <h4>
+      <h2 class="text-[24px] my-2">Information Collections</h2>
+      <h4 class="text-[18px] my-2">
         We may get two categories of information from you when you visit our
         website: personally identifiable information that you voluntarily
         disclose and that is gathered individually, as well as site use data
         that is gathered collectively when you and other users navigate our
         website.
-      </h4>
-      <ul>
-        <li>Personal Information You Provide</li>
-        <h4>
-          Registration Details: You will give us personal information about
-          yourself, including your name, email address, phone number, and
-          country, when you register to utilize our website services. Additional
-          Personally Identifiable Information that you voluntarily provide on
-          your Personal Profile page and which is not needed for registration is
-          referred to as Personal Profile Information. Sign up with a Google
-          account: First and last names, email addresses, and profile pictures
-          are collected.
-        </h4>
-        <li>Web Site Use Your Information</li>
-        <h4>
-          Our website, like many other commercial websites, gathers usage data
-          through web server log files and a common technique called "cookies"
-          (explained below under "What Are Cookies?"). The date and time of
-          visits, the pages viewed, the amount of time spent on our website, and
-          the websites visited immediately before and immediately after our
-          website are just a few of the details that can be collected by cookies
-          and Web server logs.
-        </h4>
-      </ul>
+      </h4><br>
+      <h2 class="text-[24px] my-2">Personal Information You Provide</h2>
+      <h4 class="text-[18px] my-2">
+        Registration Details: You will give us personal information about
+        yourself, including your name, email address, phone number, and country,
+        when you register to utilize our website services. Additional Personally
+        Identifiable Information that you voluntarily provide on your Personal
+        Profile page and which is not needed for registration is referred to as
+        Personal Profile Information. Sign up with a Google account: First and
+        last names, email addresses, and profile pictures are collected.
+      </h4><br>
+      <h2 class="text-[24px] my-2">Web Site Use Your Information</h2>
+      <h4 class="text-[18px] my-2">
+        Our website, like many other commercial websites, gathers usage data
+        through web server log files and a common technique called "cookies"
+        (explained below under "What Are Cookies?"). The date and time of
+        visits, the pages viewed, the amount of time spent on our website, and
+        the websites visited immediately before and immediately after our
+        website are just a few of the details that can be collected by cookies
+        and Web server logs.
+      </h4><br>
 
-      <h3>What Are Cookies?</h3>
-      <h4>
+      <h3 class="text-[24px] my-2">What Are Cookies?</h3>
+      <h4 class="text-[18px] my-2">
         A cookie is a little text document that frequently has a unique,
         anonymous identification. A website's computer requests permission from
         your computer to store this file in a location on your hard drive set
@@ -65,9 +63,11 @@
         browsers are configured to accept cookies. You can, however, change your
         browser's settings to reject cookies if you would like not to receive
         them. Should you decide to have your
-      </h4>
-      <h3>What we do with the information we collect</h3>
-      <h4>
+      </h4><br>
+      <h3 class="text-[24px] my-2">
+        What we do with the information we collect
+      </h3>
+      <h4 class="text-[18px] my-2">
         In general, we use personal data to manage our business operations,
         deliver the goods and services you requested, handle payments, track
         service usage, enhance our marketing and promotional efforts, enhance
@@ -75,22 +75,22 @@
         content, and layout of our website, among other legitimate uses. These
         applications help us make our site better and more suited to your needs.
       </h4>
-      <h4>
+      <h4 class="text-[18px] my-2">
         On rare occasions, we might also utilize the data we gather to let you
         know about significant updates to our website, brand-new services, and
         exclusive deals we believe you'll love. If you would like not to receive
         these offers, you can let us know at any time by sending us an email
         using the address listed on the newsletter. Combined Data
-      </h4>
-      <h3>Your Consent</h3>
-      <h4>
+      </h4><br>
+      <h3 class="text-[24px] my-2">Your Consent</h3>
+      <h4 class="text-[18px] my-2">
         You agree to the collection and use of your personal information by
         using our website in the ways specified in this privacy statement. This
         privacy statement is subject to change at any moment, with or without
         prior notice, per our reserves.
-      </h4>
-      <h3>Modifications to this Policy</h3>
-      <h4>
+      </h4><br>
+      <h3 class="text-[24px] my-2">Modifications to this Policy</h3>
+      <h4 class="text-[18px] my-2">
         The owners of Go-kh Free Classifieds are free to modify this policy at
         any moment. Please periodically check this page for updates. If we post
         updates to these terms, you will be deemed to have accepted them by
@@ -102,6 +102,4 @@
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
--- pages/terms-conditions/index.vue
+++ pages/terms-conditions/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex justify-center p-10 items-start h-screen">
     <div class="w-[90%] h-[100%] text-gray-500 font-bold">
-      <h1 align="center">About Us</h1>
-      <h4>
+      <h1 class="text-[32px] mt-2" align="center">Terms & Conditions</h1>
+      <h1 class="text-[24px] mb-2" align="center">About Us</h1>
+      <h4 class="text-[18px] my-2">
         Welcome to Go-kh. Thanks for using our platform. Please read these Terms
         and Conditions before using any of our services.
       </h4>
-      <h4>
+      <h4 class="text-[18px] my-2">
         By using our services, you agree to be bound by these Terms and
         Conditions. You should visit our Terms and Conditions regularly to make
         note of any changes we have made to our Terms and Conditions.
       </h4>
+      <br>
 
-      <h2>Digital Communications</h2>
-      <h4>
+      <h2 class="text-[24px] my-2">Digital Communications</h2>
+      <h4 class="text-[18px] my-2">
         You may be corresponding with us electronically when you utilize our
         services or send us emails, texts, and other correspondence from your
         desktop or mobile device. By using this platform or any other Little
@@ -24,26 +26,26 @@
         any legal requirement that these communications be made in writing is
         satisfied by all agreements, notices, disclosures, and other
         communications that we send to you electronically.
-      </h4>
+      </h4><br>
 
-      <h2>Intellectual Property Complaint</h2>
-      <h4>
+      <h2 class="text-[24px] my-2">Intellectual Property Complaint</h2>
+      <h4 class="text-[18px] my-2">
         We respect the intellectual property of the others. However, we are
         unable to detect every time if any intellectual property right is being
         infringed as our platforms are open to multiple merchants. If you
         believe that your intellectual property right is being infringed, please
         contact us at info.go-kh.com We will assist you in removing violations
         and preventing future violations.
-      </h4>
+      </h4><br>
 
-      <h2>Personal Data Protection</h2>
-      <h4>
+      <h2 class="text-[24px] my-2">Personal Data Protection</h2>
+      <h4 class="text-[18px] my-2">
         Please see our Privacy Policy, which forms a part of these Terms and
         Conditions.
-      </h4>
+      </h4><br>
 
-      <h2>Accessibility and Limitation</h2>
-      <h4>
+      <h2 class="text-[24px] my-2">Accessibility and Limitation</h2>
+      <h4 class="text-[18px] my-2">
         As a condition of use, you promise not to use the services for any
         purpose that is unlawful or prohibited by these Terms, or any other
         purpose not reasonably intended by Little Digital. By way of example,
@@ -52,9 +54,9 @@
         creating problems. To communicate with Little Digital representatives or
         other users in abusive or offensive manner. To create multiple accounts
         for the purpose of exploiting our system
-      </h4>
-      <h2>Notifications</h2>
-      <h4>
+      </h4><br>
+      <h2 class="text-[24px] my-2">Notifications</h2>
+      <h4 class="text-[18px] my-2">
         These Terms & Conditions are subject to change at any moment, at our
         discretion. Any changes made to these terms and conditions will be
         available on the internet. Nonetheless, using the website going forward
