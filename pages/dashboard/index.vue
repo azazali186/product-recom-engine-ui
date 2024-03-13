59c9ca4dcf6034b166aa899c6f8606fc80a20de0
Janny <yenny.maya186@gmail.com>
2024-03-13 18:29:23 +0700

    Updated login button

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,8 @@
 ﻿<template>
   <h1>
-    Thank you for register Login using below button.
+    Thank you for register Login using button.
     <ULink to="https://admin.go-kh.com" target="_blank">
-      <Button> Login </Button>
+      <Button class="bg-red-500 text-white px-5 py-3 "> Login </Button>
     </ULink>
   </h1>
 </template>
